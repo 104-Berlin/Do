14a8269692bd65149e4e109d10171192f1156375
J4m3s00 <james.lol2000@gmail.com>
2020-07-12 21:15:12 +0200

    Maybe this could work

--- scripts/build.py
+++ scripts/build.py
@@ -7,8 +7,4 @@ os.chdir(homepath)
 os.system("mkdir build")
 os.chdir("build")
 os.system("cmake ..")
-
-if (os.sys.platform == "win32"):
-    os.system("msbuild Do_Scripting.sln")
-else:
-    os.system("make")
+os.system("cmake --build .")
