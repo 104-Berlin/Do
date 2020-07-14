555ea06d395da8cf238f3afe785e2a0dfe636b3b
J4m3s00 <james.lol2000@gmail.com>
2020-07-14 22:58:13 +0200

    Update prefix_plugin_loader.h

--- core/plugin_loader/prefix_plugin_loader.h
+++ core/plugin_loader/prefix_plugin_loader.h
@@ -1,6 +1,9 @@
 #pragma once
-#include <dlfcn.h>
 #include <stdlib.h>
+
+#ifndef _WIN
+#include <dlfcn.h>
+#endif
 
 DO_DEFINE_ERROR(DO_PLUGIN_LOADFAIL, "Could not find or load the plugin from the path!")
 DO_DEFINE_ERROR(DO_NO_LOAD_FN, "Could not find the initialization function of the plugin!")
