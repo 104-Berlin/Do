d834ea8beb27af928575fe0835f8f63abd4ef816
J4m3s00 <james.lol2000@gmail.com>
2020-07-15 00:15:28 +0200

    Update plugin_loader.c

--- core/plugin_loader/plugin_loader.c
+++ core/plugin_loader/plugin_loader.c
@@ -18,10 +18,13 @@ do_err do_load_plugin_func(t_do_plugin plugin, const char* plugin_name, t_load_f
 {
     if (!plugin) { return DO_INVALID_PLUGIN; }
 
+    #ifdef _MAC
     t_load_fn load_function = (t_load_fn) dlsym(plugin, DO_LOAD_PLUGIN_FUNCTION_NAME);
     if (!load_function) { fprintf(stderr, "%s\n", dlerror()); return DO_NO_LOAD_FN; }
-
     *o_func = load_function;
+    //TODO Add other platforms
+    #endif
+
 
     return DO_NOERROR;
 }
