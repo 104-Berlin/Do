1f006a93ef800a266d8306e9bb020b87479b2d5f
J4m3s00 <james.lol2000@gmail.com>
2020-07-14 20:46:27 +0200

    Add Preprocessors for mac and windows

--- core/prefix.h
+++ core/prefix.h
@@ -1,9 +1,16 @@
+#pragma once
+
+// STANDARD INCLUDES
 #include <stdio.h>
+#include <stdint.h>
+
+typedef uint8_t     u8;
+typedef uint16_t    u16;
+typedef uint32_t    u32;
 
 
-typedef char* do_err;
-do_err __last_error = 0;             // WATCH THESE NEED TO BE SYNC. SO DONT SWITCH THE TYPES CARELESS
-
+typedef char *do_err;
+do_err __last_error = 0; // WATCH THESE NEED TO BE SYNC. SO DONT SWITCH THE TYPES CARELESS
 
 #define DO_DEFINE_ERROR(errDsc, errString) do_err errDsc = errString;
 #define DO_GETERR __last_error
@@ -12,7 +19,18 @@ DO_DEFINE_ERROR(DO_NOERROR, "NO Error")
 DO_DEFINE_ERROR(DO_ANYERROR, "Unkown Error")
 DO_DEFINE_ERROR(DO_TESTERROR, "This is shit and should be removed later!")
 
-
-#define DO_SUCCEED(fn, code) if ((__last_error = fn) == DO_NOERROR) {code} else {printf("ERROR: %s\n", DO_GETERR);}
+#define DO_SUCCEED(fn, code)               \
+    if ((__last_error = fn) == DO_NOERROR) \
+    {                                      \
+        code                               \
+    }                                      \
+    else                                   \
+    {                                      \
+        printf("ERROR: %s\n", DO_GETERR);  \
+    }
 
 // Other error codes. Allway return them in funciton calls
+
+// Includes
+
+#include "plugin_loader/prefix_plugin_loader.h"
