02ba6c3ebe0c80452994407a3edfa48772f0cf2e
J4m3s00 <james.lol2000@gmail.com>
2020-07-14 22:55:39 +0200

    build err

--- core/plugin_loader/plugin_loader.c
+++ core/plugin_loader/plugin_loader.c
@@ -8,7 +8,7 @@ do_err do_load_plugin(const char* plugin_name, t_load_fn* oFn)
     if (!lib_handle) { fprintf(stderr, "%s\n", dlerror()); return DO_PLUGIN_LOADFAIL; }
 
     t_load_fn load_function = (t_load_fn) dlsym(lib_handle, DO_LOAD_PLUGIN_FUNCTION_NAME);
-    if (!load_function) { fprintf(stderr, "%s\n", dlerror()) return DO_NO_LOAD_FN; }
+    if (!load_function) { fprintf(stderr, "%s\n", dlerror()); return DO_NO_LOAD_FN; }
 
     *oFn = load_function;
     #elif _WIN
