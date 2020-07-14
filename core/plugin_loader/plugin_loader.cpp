db19a34a23ffbc4ebd1d420f433710c3777f33ca
J4m3s00 <james.lol2000@gmail.com>
2020-07-14 20:47:50 +0200

    Create one func

---
+++ core/plugin_loader/plugin_loader.cpp
@@ -0,0 +1,11 @@
+#include "prefix.h"
+
+
+do_err do_load_plugin(const char* plugin_name)
+{
+    #ifdef _MAC
+        
+    #elif _WIN
+    #elif _LIN
+    #endif
+}
---
+++ core/plugin_loader/plugin_loader.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+ * 
+ * Loads a plugin to memory 
+ * 
+ * 
+ * 
+ * 
+ */
+extern do_err do_load_plugin(const char* plugin_name);
